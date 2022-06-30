e273d787c61b6488a6e7a822f5b90ccaed75863e
Mahendra Choudhary <mahendrachoudhary.in@gmail.com>
2022-06-30 20:54:50 +0530

    env updated

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,16 @@
-module.exports = {
-    HOST: "127.0.0.1",
-    USER: "root",
-    PASSWORD: "TechAdmin@911",
-    DB: "ecom_db",
-    dialect: "mysql" //RDBMS being used. 
+module.exports = { 
+    development: {
+        HOST: "127.0.0.1",
+        USER: "root",
+        PASSWORD: "TechAdmin@911",
+        DB: "ecom_db",
+        dialect: "mysql" //RDBMS being used. 
+    },
+    production: {
+        HOST: "sql6.freemysqlhosting.net",
+        USER: "sql6503166",
+        DB: "sql6503166",
+        PASSWORD: "WrjPMHCNjA",
+        dialect: "mysql" //RDBMS being used. 
+    }
 }
